5687b7ad3a07fcbc86c61e66ecb223b8dbca4158
Michael Kahn <kahn.mp@gmail.com>
2021-03-23 21:12:54 -0700

    adding routes

---
+++ Develop/controller/apis/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const [ROUTE IN THIS FOLDER] = require('./ROUTE IN THIS FOLDER');
+// const {ROUTE IN THIS FOLDER} = require('./ROUTE IN THIS FOLDER');
+
+// router.use('/users', userRoutes);
+// router.use('/projects', projectRoutes);
+
+module.exports = router;
