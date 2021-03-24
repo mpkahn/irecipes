4d128638971dd0b0905bcefabac0528f8e551df9
dustin.knowles <dustin.knowles22@gmail.com>
2021-03-23 21:33:51 -0700

    adding server listen functionality

--- Develop/server.js
+++ Develop/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const routes = require('./controller/homeRoutes');
+const helpers = require('./utils/helper');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
